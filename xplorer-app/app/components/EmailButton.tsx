381a1949b638589cf0278a41fe756cf034e0d9ce
KarsCode <kartiksubbaram18@gmail.com>
2023-10-16 17:04:02 +0530

    Mailing functions added to be implemented.

---
+++ xplorer-app/app/components/EmailButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from 'react';
+
+const Button = () => {
+  const handleSendEmail = async () => {
+    try {
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        console.log('Email sent successfully');
+      } else {
+        console.error('Failed to send Email');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Send an email:</h1>
+      <button onClick={handleSendEmail}>Here We Go</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ xplorer-app/pages/api/sendEmail.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          user: 'RAM_page123@outlook.com', // Replace with your Gmail email
+          pass: 'RAMpage@123', // Replace with your Gmail password or an app-specific password
+        },
+      });
+
+      const mailOptions = {
+        from: 'RAM_page123@outlook.com',
+        to: 'jay.jithendra@gmail.com', // Replace with the recipient's email address
+        subject: 'Hello from My Email App',
+        text: 'Its a good day tpo learn NextJS.',
+      };
+
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent:', info.response);
+
+      res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ error: 'Failed to send email' });
+    }
+  } else {
+    res.status(405).end();
+  }
+}
