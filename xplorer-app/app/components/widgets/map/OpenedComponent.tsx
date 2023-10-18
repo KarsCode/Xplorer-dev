e6910b1028c195092291f14793e685dfb7d225aa
KarsCode <kartiksubbaram18@gmail.com>
2023-10-18 17:38:30 +0530

    zoom functionality added

--- xplorer-app/app/components/widgets/map/OpenedComponent.tsx
+++ xplorer-app/app/components/widgets/map/OpenedComponent.tsx
@@ -13,7 +13,11 @@ function OpenedComponent({ currentUser ,closeModal }: OpenedComponentProps) {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-opacity-60 bg-black z-10">
       <div className="bg-transparent p-10 border-0 text-center relative w-screen h-screen max-w-7xl box-border flex flex-col justify-center items-center">
-        <h2> <MapComponent currentUser={currentUser}/></h2>
+        
+          
+           <MapComponent currentUser={currentUser}/>
+           
+        
         <button onClick={closeModal} className='text-white bg-black-500 hover:bg-gray-600 py-2 px-4 rounded'>Close</button>
       </div>
     </div>
